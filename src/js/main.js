351a324777c38c7751951a78ce230015e818925c
natali0258 <natali0258@yandex.ru>
2023-06-08 01:03:14 +0700

    Для экранов меньше 768px добавлено бургер-меню

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,6 @@
 let btn = $('.first-screen__btn');
 let title = $('.first-screen__title');
 let subtitle = $('.first-screen__subtitle')
-//console.log(btn, title)
 
 //при клике на кнопку
 btn.on('click', function () {
@@ -11,9 +10,21 @@ btn.on('click', function () {
 });
 
 btn.on('mouseover', function () {
-   btn.css('background-color', 'red')
+   btn.css('background-color', '#8A8A8A')
 });
 
 btn.on('mouseout', function () {
    btn.css('background-color', 'transparent')
 });
+
+//управление бургер-меню:
+let burger = $('.burger');
+let navigation = $('.header__nav');
+let dublicate = $('.header__title_dublicate');
+
+burger.on('click', function () {
+   burger.toggleClass('burger_active')
+   navigation.toggleClass('header__nav_active')
+   dublicate.toggleClass('header__title_dublicate_active')
+})
+
